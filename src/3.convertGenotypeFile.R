8d5e2d0c00721b34d9985c7c846bdbd71d621e76
gziegler <gziegler@danforthcenter.org>
2018-11-16 13:38:21 -0600

    Adding some status messages during run

--- src/3.convertGenotypeFile.R
+++ src/3.convertGenotypeFile.R
@@ -12,9 +12,9 @@ hetThreshold <- 0.25 #Because of the density of SNPs, I REMOVE SNPs with a het v
 maf <- 0.1
 maxMissingBySNP <- 0.2
 
-geno <- fread(paste0("../data/genotype/2.from12.setaria.maf0.1.maxMissing0.1.012"),header=FALSE,sep="\t")
-indv <- read.table(paste0("../data/genotype/2.from12.setaria.maf0.1.maxMissing0.1.012.indv"),sep="\t",header=FALSE,stringsAsFactors = FALSE)
-snpInfo <- fread(paste0("../data/genotype/2.from12.setaria.maf0.1.maxMissing0.1.012.pos"),sep="\t",header=FALSE,stringsAsFactors = FALSE)
+geno <- fread(paste0("../data/genotype/2.from12.setaria.maf0.1.maxMissing0.1.allLines.012"),header=FALSE,sep="\t")
+indv <- read.table(paste0("../data/genotype/2.from12.setaria.maf0.1.maxMissing0.1.allLines.012.indv"),sep="\t",header=FALSE,stringsAsFactors = FALSE)
+snpInfo <- fread(paste0("../data/genotype/2.from12.setaria.maf0.1.maxMissing0.1.allLines.012.pos"),sep="\t",header=FALSE,stringsAsFactors = FALSE)
 setnames(snpInfo,c("V1","V2"),c("chr","pos"))
 #First column of geno is  rownumbers 0:length(lines)
 geno$V1 <- indv$V1
@@ -23,15 +23,15 @@ setnames(geno,colnames(geno),c("Genotype",paste0(snpInfo$chr,"_",snpInfo$pos)))
 geno <- transpose(geno)
 setnames(geno, old = names(geno), new = as.character(geno[1,]))
 geno <- geno[-1,]
-
+message("Finished reading genotype files. Changing missing (-1) to NA.")
 ###Change -1 values to missing
 for(j in seq_len(ncol(geno))){
   set(geno,which(geno[[j]]==-1),j,NA)
 }
-
+message("There are ",nrow(geno)," snps.")
 ##geno now has columns as individuals, rows are SNPs, there are no metadata columns
 alleleTable <- cbind(snpInfo,ionomicsUtils::calcHet(geno))
-
+message("Finished calculating het calls. Now calculating missing and minor allele frequency.")
 # alleleTable[,discard := (FracHet>=hetThreshold)]
 # if(length(which(alleleTable$discard==TRUE))>0){
 #   geno[which(alleleTable$discard==TRUE)][geno[which(alleleTable$discard==TRUE)]==1] <- NA
@@ -42,7 +42,8 @@ alleleTable <- cbind(snpInfo,ionomicsUtils::calcHet(geno))
 alleleTable <- cbind(alleleTable,ionomicsUtils::calcMAF(geno))
 #decide who to delete
 alleleTable[,discard := (FracHet>hetThreshold | FracMissing>maxMissingBySNP | MAF<maf | MAF>(1-maf))]
-
+numDiscard <- length(which(alleleTable$discard==TRUE))
+message("Finished calculating MAF: ",numDiscard," SNPs being discarded.")
 #get rid of the rows in snp that are TRUE in alleleTable$discard
 keepIdxs <- which(alleleTable$discard==FALSE)
 cols <- names(geno)
@@ -70,7 +71,7 @@ alleleTable[, discard := NULL]
 
 
 save(snp.subset,alleleTable,file="../data/genotype/3.FilteredGenotypeFile.hetFilter0.25.maf0.1.rda")
-
+message("SNPs discarded and data.table file saved. Converting to matrix.")
 #we can now convert to a matrix and use the apply functions
 genoMatrix <- as.matrix(snp.subset)
 rm(snp.subset)
@@ -78,8 +79,9 @@ class(genoMatrix) <- "numeric"
 
 alleleTable[, chr := sapply(strsplit(alleleTable$chr, "_"), "[", 2)]
 alleleTable[, chr := as.numeric(chr)]
-
+message("Matrix conversion successul. Removing scaffold SNPs.")
 keepIdxs <- which(alleleTable$chr %in% c(1:9)) #matches first 1:4420660 indices identical(keepIdxs[1:4236621],c(1:4236621))
+message((nrow(alleleTable)-length(keepIdxs))," Scaffold snps removed. ", length(keepIDxs)," remaining.")
 genoMatrix <- genoMatrix[keepIdxs,]
 alleleTable <- alleleTable[keepIdxs,]
 rm(keepIdxs)
@@ -91,7 +93,7 @@ alleleTable <- alleleTable[keepIdxs,]
 rm(keepIdxs)
 
 save(genoMatrix,alleleTable,file="../data/genotype/3.FilteredGenotypeFile.MatrixFormat.noscaffold.hetFilter0.25.maf0.1.rda")
-
+message("Matrix format table saved. Finished.")
 #neighborDists <- neighborDists[keepIdxs]
 #neighborDists <- neighborDists[-length(neighborDists)]
 #neighborCors <- neighborCors[keepIdxs]
