5c86cbbcc2f5de516604c2626f0dd6f0d7c98b07
gziegler <gziegler@danforthcenter.org>
2018-11-16 11:33:43 -0600

    Updating pipeline to do an intial filter using all lines

--- src/1.getSetariaHapmap.bash
+++ src/1.getSetariaHapmap.bash
@@ -1,3 +1,3 @@
 wget https://www.dropbox.com/s/s7d8l3ixvk8b6nu/12.Setaria_598g_8.58M_withRef_imp_phased_maf0.01_FINAL.vcf.bz2?dl=0
 mv 12.Setaria_598g_8.58M_withRef_imp_phased_maf0.01_FINAL.vcf.bz2\?dl\=0 ../data/genotype/12.Setaria_598g_8.58M_withRef_imp_phased_maf0.01_FINAL.vcf.bz2
-bunzip2 12.Setaria_598g_8.58M_withRef_imp_phased_maf0.01_FINAL.vcf.bz2
+bunzip2 ../data/genotype/12.Setaria_598g_8.58M_withRef_imp_phased_maf0.01_FINAL.vcf.bz2
