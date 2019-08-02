a73f3b6c521d240ac50a759bdb75469ca007ae8f
gziegler <gziegler@danforthcenter.org>
2019-08-02 15:26:15 -0500

    Updating file names to 10. from 7.

--- src/10.runMLMM_cof.VCF.bash
+++ src/10.runMLMM_cof.VCF.bash
@@ -1,30 +1,30 @@
-Rscript ./7.runMLMM_cof.VCF.R &
-Rscript ./7.runMLMM_cof.VCF.R &
+Rscript ./10.runMLMM_cof.VCF.R &
+Rscript ./10.runMLMM_cof.VCF.R &
   sleep 5
-Rscript ./7.runMLMM_cof.VCF.R &
+Rscript ./10.runMLMM_cof.VCF.R &
   sleep 5
-Rscript ./7.runMLMM_cof.VCF.R &
+Rscript ./10.runMLMM_cof.VCF.R &
   sleep 5
-Rscript ./7.runMLMM_cof.VCF.R &
+Rscript ./10.runMLMM_cof.VCF.R &
   sleep 5
-Rscript ./7.runMLMM_cof.VCF.R &
+Rscript ./10.runMLMM_cof.VCF.R &
   sleep 5
-Rscript ./7.runMLMM_cof.VCF.R &
+Rscript ./10.runMLMM_cof.VCF.R &
   sleep 5
-Rscript ./7.runMLMM_cof.VCF.R &
+Rscript ./10.runMLMM_cof.VCF.R &
   sleep 5
-Rscript ./7.runMLMM_cof.VCF.R &
+Rscript ./10.runMLMM_cof.VCF.R &
   sleep 5
-Rscript ./7.runMLMM_cof.VCF.R &
+Rscript ./10.runMLMM_cof.VCF.R &
   sleep 5
-Rscript ./7.runMLMM_cof.VCF.R &
+Rscript ./10.runMLMM_cof.VCF.R &
   sleep 5
-Rscript ./7.runMLMM_cof.VCF.R &
+Rscript ./10.runMLMM_cof.VCF.R &
   sleep 5
-Rscript ./7.runMLMM_cof.VCF.R &
+Rscript ./10.runMLMM_cof.VCF.R &
   sleep 5
-Rscript ./7.runMLMM_cof.VCF.R &
+Rscript ./10.runMLMM_cof.VCF.R &
   sleep 5
-Rscript ./7.runMLMM_cof.VCF.R &
+Rscript ./10.runMLMM_cof.VCF.R &
   wait
 echo all jobs are done!
