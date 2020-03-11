9d3fb8e644add7fce18ead75fa519355cb3b9cf5
gziegler <gziegler@danforthcenter.org>
2020-03-11 15:17:46 -0500

    Minor updates

--- src/11.mergeRDAfiles.R
+++ src/11.mergeRDAfiles.R
@@ -1,15 +1,21 @@
 rm(list=ls())
 library(data.table)
 library(dplyr)
-
+library(feather)
+###To save time this doesn't calcualte fisherP for every SNP, it first filters any SNPs where the minimum pvalue (across all phenotypes) is > 0.1
+#####A pvalue of 0.06 across 3 experiments gives a fisher p of 0.0097
+######This actually only removes about 5000 SNPs for IB005,IB007,IB008
+##So ended up not doing it
+#calcFisher <- 0.06 #threshold to filter SNPs before calculating fisher's
 #first pass fisherP threshold - filter SNPs by this fisherP, next pass will clump remaining SNPs and select best p-val from clumps
 firstThresh <- 1e-2
 
 #library(metap) #for fisher's combined
 resultsFiles <- list.files("../GWASresults/","\\.rda",full.names=TRUE)
-#resultsFiles <- grep("IB005|IB007|IB008",resultsFiles,value=TRUE)
+resultsFiles <- grep("10.mlmmResults",resultsFiles,value=TRUE)
+resultsFiles <- grep("IB005|IB007|IB008",resultsFiles,value=TRUE)
 #resultsFiles <- grep("IB003|IB004",resultsFiles,value=TRUE)
-resultsFiles <- grep("rank",resultsFiles,value=TRUE)
+#resultsFiles <- grep("rank",resultsFiles,value=TRUE)
 mergeRes <- data.frame()
 for(i in resultsFiles){
   message("Reading results file ",i,"\n")
@@ -30,24 +36,44 @@ for(i in resultsFiles){
   rm(keepSNPs)
 }
 
+#can write it out here to save time later, same file + fisherP is written out below
+#save(mergeRes,file="../GWASresults/11.IB005toIB008mergedResults.rda",compress="xz",compression_level = 5)
+#arrow::write_parquet(mergeRes,sink="../GWASresults/11.IB005toIB008mergedResults.parquet",compression="gzip",compression_level = 5)
+#feather::write_feather(mergeRes,path="../GWASresults/11.IB005toIB008mergedResults.feather")
+#arrow::write_parquet(mergeRes,sink="../GWASresults/11.IB003toIB004mergedResults.parquet",compression="gzip",compression_level = 5)
+#feather::write_feather(mergeRes,path="../GWASresults/11.IB003toIB004mergedResults.feather")
+
 #Deconvulte Trait ID into a table
-traitTable <- data.frame(Phenotype = sapply(strsplit(colnames(mergeRes)[2:ncol(mergeRes)],"_"),"[",1),
-                         DAP =       as.numeric(sapply(strsplit(sapply(strsplit(sapply(strsplit(colnames(mergeRes)[2:ncol(mergeRes)],"_"),"[",2),"\\."),"[",3),"-"),"[",1)),
-                         Treatment = sapply(strsplit(sapply(strsplit(colnames(mergeRes)[2:ncol(mergeRes)],"_"),"[",2),"\\."),"[",2),
-                         Experiment = sapply(strsplit(sapply(strsplit(colnames(mergeRes)[2:ncol(mergeRes)],"_"),"[",2),"\\."),"[",1),
+traitTable <- data.frame(Phenotype = sapply(strsplit(sapply(strsplit(colnames(mergeRes)[2:ncol(mergeRes)],"_\\s*(?=[^_]+$)",perl=TRUE),"[",1),"_\\s*(?=[^_]+$)",perl=TRUE),"[",1), #regex to split on last _ (sometimes phenotypes have an '_')
+                         DAP =       as.numeric(sapply(strsplit(sapply(strsplit(sapply(strsplit(colnames(mergeRes)[2:ncol(mergeRes)],"_\\s*(?=[^_]+$)",perl=TRUE),"[",2),"\\."),"[",2),"-"),"[",1)),
+                         Treatment = sapply(strsplit(sapply(strsplit(colnames(mergeRes)[2:ncol(mergeRes)],"_\\s*(?=[^_]+$)",perl=TRUE),"[",1),"_\\s*(?=[^_]+$)",perl=TRUE),"[",2),
+                         Experiment = sapply(strsplit(sapply(strsplit(colnames(mergeRes)[2:ncol(mergeRes)],"_\\s*(?=[^_]+$)",perl=TRUE),"[",2),"\\."),"[",1),
                          colID = colnames(mergeRes)[2:ncol(mergeRes)],
                          stringsAsFactors = FALSE)
 #calculate fishers combined on each Phenotype/DAP/treatment combination
 #create function for fisher combining pvals, function from Charles Pignon, gives same answer as metap::sumlog
-fisher_combine_pvals=function(pval_list){pchisq((sum(log(pval_list))*-2), df=length(pval_list)*2, lower.tail=F)}
-mergeRes <- as.data.table(mergeRes)
+#fisher_combine_pvals=function(pval_list){pchisq((sum(log(pval_list))*-2), df=length(pval_list)*2, lower.tail=F)} #calculating this in data.table is 
+###much, much faster
+# message(nrow(mergeRes)," SNPs before first filter.")
+# setDT(mergeRes)
+# pvalCols <- grep("pval",colnames(mergeRes),value=TRUE)
+# mergeRes[,row.min := do.call(pmin, c(.SD,na.rm = TRUE)),.SDcols=pvalCols]
+# firstFilter <- mergeRes[row.min < calcFisher] #keep rows where minimum is less than the threshold
+# firstFilter$row.min <- NULL
+# message(nrow(firstFilter)," SNPs after first filter.")
+# mergeRes <- firstFilter
+# rm(firstFilter)
+#For all of the lemnatec traits, this apparently ended up removing almost no SNPs
+setDT(mergeRes)
 for(pheno in unique(traitTable$Phenotype)){
   for(dap in unique(traitTable$DAP[which(traitTable$Phenotype==pheno)])){
     for(treat in unique(traitTable$Treatment[which(traitTable$Phenotype==pheno & traitTable$DAP==dap)])){
       message(pheno," ",dap," ",treat,"\n")
       thisTraitCols <- grep("pval",traitTable$colID[which(traitTable$Phenotype==pheno & traitTable$DAP==dap & traitTable$Treatment==treat)],value=TRUE)
+      chiSqDF = 2*length(thisTraitCols)
       #thisTraitCols <- grep("mean",thisTraitCols,value=TRUE,invert = TRUE) #exclude columns from rank.mean experiment
-      mergeRes[,paste(pheno,treat,dap,"fisherP",sep="_") := fisher_combine_pvals(.SD),by=1:nrow(mergeRes),.SDcols = thisTraitCols]
+      mergeRes[,paste(pheno,treat,dap,"fisherP",sep="_") := pchisq((-2*rowSums(log(.SD),na.rm=T)),df=chiSqDF,lower.tail=F), .SDcols = thisTraitCols]
+      #mergeRes[,paste(pheno,treat,dap,"fisherP",sep="_") := fisher_combine_pvals(.SD),by=seq_len(nrow(mergeRes)),.SDcols = thisTraitCols]
     }
   }
 }
@@ -56,14 +82,16 @@ fisherPcols <- grep("fisherP",colnames(mergeRes),value=TRUE)
 #fisherPcols <- grep("fisherP|mean.*pval",colnames(mergeRes),value=TRUE) #don't filter out mean pval column (for the rank dataset)
 filterRes <- mergeRes[mergeRes[, Reduce(`|`, lapply(.SD, `<=`, firstThresh)),.SDcols = fisherPcols]] #keep phenotype columns where fisherP <= firstThresh
 #save(mergeRes,file=paste0("../GWASresults/11.CombinedMLMMResultsWithFisherPval.allSNPs.rda"),compress="xz",compression_level = 5)
-#save(filterRes,file=paste0("../GWASresults/11.CombinedMLMMResultsWithFisherPval.filtered.rda"),compress="xz",compression_level = 5)
+#save(filterRes,file=paste0("../GWASresults/11.allLTphenos.IB005to8.CombinedMLMMResultsWithFisherPval.filtered.rda"),compress="xz",compression_level = 5)
+#feather::write_feather(mergeRes,path="../GWASresults/11.allLTphenos.IB003to4.CombinedMLMMResultsWithFisherPval.allSNPs.feather")
+feather::write_feather(mergeRes,path="../GWASresults/11.allLTphenos.IB005to8.CombinedMLMMResultsWithFisherPval.allSNPs.feather")
 
 #save(mergeRes,file=paste0("../GWASresults/11.JustIB005to8.CombinedMLMMResultsWithFisherPval.allSNPs.rda"),compress="xz",compression_level = 5)
 #save(filterRes,file=paste0("../GWASresults/11.JustIB005to8.CombinedMLMMResultsWithFisherPval.filtered.rda"),compress="xz",compression_level = 5)
 # save(mergeRes,file=paste0("../GWASresults/11.JustIB003to4.CombinedMLMMResultsWithFisherPval.allSNPs.rda"),compress="xz",compression_level = 5)
 # save(filterRes,file=paste0("../GWASresults/11.JustIB003to4.CombinedMLMMResultsWithFisherPval.filtered.rda"),compress="xz",compression_level = 5)
-save(mergeRes,file=paste0("../GWASresults/11.RANK5to8.CombinedMLMMResultsWithFisherPval.allSNPs.rda"),compress="xz",compression_level = 5)
-save(filterRes,file=paste0("../GWASresults/11.RANK5to8.CombinedMLMMResultsWithFisherPval.filtered.rda"),compress="xz",compression_level = 5)
+#save(mergeRes,file=paste0("../GWASresults/11.RANK5to8.CombinedMLMMResultsWithFisherPval.allSNPs.rda"),compress="xz",compression_level = 5)
+#save(filterRes,file=paste0("../GWASresults/11.RANK5to8.CombinedMLMMResultsWithFisherPval.filtered.rda"),compress="xz",compression_level = 5)
 
 ####Move on to clump SNPs####
 
--- src/13.mergeClumpResultsRecalcFisher.R
+++ src/13.mergeClumpResultsRecalcFisher.R
@@ -8,28 +8,42 @@ dataList <- list()
 for(i in resultsFiles){
   message("Reading results file ",i,"\n")
   dataList[[length(dataList)+1]] <- feather::read_feather(path=i)
+  #add max of previous clump size to current clump
+  if(length(dataList) != 1){
+    dataList[[length(dataList)]]$clump <- dataList[[length(dataList)]]$clump+max(dataList[[length(dataList)-1]]$clump)
+  }
 }
 snpTable <- data.table::rbindlist(dataList)
 rm(dataList)
 
+#Open and merge IB0005 with snpTable clump information
+IB005 <- feather::read_feather(path="../GWASresults/11.allLTphenos.IB005to8.CombinedMLMMResultsWithFisherPval.allSNPs.feather")
+IB005merge <- merge(IB005,snpTable[,c("SNP","clump","chrom","pos")],by="SNP",all=T)
+# #Add chromsome and range to each SNP
+# IB005merge$chrom <- as.numeric(sapply(strsplit(IB005merge$SNP,"_"),"[",1))
+# IB005merge$pos <- as.numeric(sapply(strsplit(IB005merge$SNP,"_"),"[",2))
+rm(IB005)
+feather::write_feather(IB005merge,path="../GWASresults/13.clumpColumn.IB005to8.CombinedMLMMResultsWithFisherPval.allSNPs.feather")
 
 pvalCols <- grep("pval|fisherP",colnames(snpTable),value=TRUE)
-
+setDT(snpTable)
 summariseSNP <- snpTable[, lapply(.SD, min,na.rm=TRUE),by=clump,.SDcols = pvalCols] #get minimum pvalue for each snp from each clump
 
-#Add chromsome and range to each SNP
+
+
 clumpPos <- snpTable %>% group_by(clump) %>% dplyr::summarise(chrom=min(chrom),minBP=min(pos),maxBP=max(pos),nSNPs=dplyr::n())
 summariseSNP <- merge(clumpPos,summariseSNP,by="clump")
 setDT(summariseSNP)
 #recalculate fisher pvals
 #Deconvulte Trait ID into a table
 pvalCols <- grep("pval",colnames(snpTable),value=TRUE)
-traitTable <- data.frame(Phenotype = sapply(strsplit(pvalCols,"_\\s*(?=[^_]+$)",perl=TRUE),"[",1), #regex to split on last _ (sometimes phenotypes have an '_')
-                         DAP =       as.numeric(sapply(strsplit(sapply(strsplit(sapply(strsplit(pvalCols,"_\\s*(?=[^_]+$)",perl=TRUE),"[",2),"\\."),"[",3),"-"),"[",1)),
-                         Treatment = sapply(strsplit(sapply(strsplit(pvalCols,"_\\s*(?=[^_]+$)",perl=TRUE),"[",2),"\\."),"[",2),
+traitTable <- data.frame(Phenotype = sapply(strsplit(sapply(strsplit(pvalCols,"_\\s*(?=[^_]+$)",perl=TRUE),"[",1),"_\\s*(?=[^_]+$)",perl=TRUE),"[",1), #regex to split on last _ (sometimes phenotypes have an '_')
+                         DAP =       as.numeric(sapply(strsplit(sapply(strsplit(sapply(strsplit(pvalCols,"_\\s*(?=[^_]+$)",perl=TRUE),"[",2),"\\."),"[",2),"-"),"[",1)),
+                         Treatment = sapply(strsplit(sapply(strsplit(pvalCols,"_\\s*(?=[^_]+$)",perl=TRUE),"[",1),"_\\s*(?=[^_]+$)",perl=TRUE),"[",2),
                          Experiment = sapply(strsplit(sapply(strsplit(pvalCols,"_\\s*(?=[^_]+$)",perl=TRUE),"[",2),"\\."),"[",1),
                          colID = pvalCols,
                          stringsAsFactors = FALSE)
+
 
 for(pheno in unique(traitTable$Phenotype)){
   for(dap in unique(traitTable$DAP[which(traitTable$Phenotype==pheno)])){
@@ -51,12 +65,35 @@ filterRes <- summariseSNP[summariseSNP[, Reduce(`|`, lapply(.SD, `<=`, finalThre
 ###Use this when there are more than one trait
 summariseMelt <- melt(filterRes[,c("clump","chrom","minBP","maxBP",clumpCols),with=FALSE],id.vars = c("clump","chrom","minBP","maxBP"))
 summariseMelt$fisherLogP <- -log10(summariseMelt$value)
-filteredSNP <- summariseMelt[summariseMelt$fisherLogP >= 7,]
+####Rather than filter by stringent p-value, keep top 50 (with a p < final thresh) from each phenotype/dap/treat###
+nKeep <- 50
+filteredSNP <- summariseMelt %>% group_by(variable) %>% top_n(n=nKeep,wt=fisherLogP)
+filteredSNP <- filteredSNP[filteredSNP$fisherLogP >= -log10(finalThresh),]
+
 filteredSNP$Phenotype = sapply(strsplit(sapply(strsplit(sapply(strsplit(as.character(filteredSNP$variable),"_\\s*(?=[^_]+$)",perl=TRUE),"[",1),"_\\s*(?=[^_]+$)",perl=TRUE),"[",1),"_\\s*(?=[^_]+$)",perl=TRUE),"[",1) #regex to split on last _ (sometimes phenotypes have an '_')
 filteredSNP$DAP = as.numeric(sapply(strsplit(sapply(strsplit(as.character(filteredSNP$variable),"_\\s*(?=[^_]+$)",perl=TRUE),"[",1),"_\\s*(?=[^_]+$)",perl=TRUE),"[",2))
 filteredSNP$Treatment = sapply(strsplit(sapply(strsplit(sapply(strsplit(as.character(filteredSNP$variable),"_\\s*(?=[^_]+$)",perl=TRUE),"[",1),"_\\s*(?=[^_]+$)",perl=TRUE),"[",1),"_\\s*(?=[^_]+$)",perl=TRUE),"[",2)
 filteredSNP$clumpCenter = round((filteredSNP$minBP+filteredSNP$maxBP)/2)
+#filteredSNP$Experiment = "IB003toIB004"
 filteredSNP$Experiment = "IB005toIB008"
 
-write.table(filterRes,file="../GWASresults/12.clumpColumn.GWASresults.fisherCombinedPval.LTtraits.IB005toIB008.csv",sep=",",col.names=TRUE,row.names=FALSE)
-write.table(filteredSNP,"../results/12.clumpColumn.GWASresults.filtered.ZbrowseReady.IB005toIB008.csv",sep=",",col.names=TRUE,row.names=FALSE)
+#IB003 <- filteredSNP
+
+finalSNPListAllExps <- rbindlist(list(IB003,filteredSNP))
+
+####Change DAP for single trait measurments to 1 more than the maximum DAP####
+single.measure.traits <- c("area.slope",
+                           "CR_num",
+                           "CR.Angle",
+                           "d13C",
+                           "CN_ratio",
+                           "gN.m2",
+                           "gC.m2",
+                           "leaf.area",
+                           "leaf.weight",
+                           "spec.leaf.area",
+                           "stom.density")
+finalSNPListAllExps$DAP[finalSNPListAllExps$Phenotype %in% single.measure.traits] <- max(finalSNPListAllExps$DAP)+1
+
+#write.table(filterRes,file="../GWASresults/12.clumpColumn.GWASresults.fisherCombinedPval.LTtraits.IB005toIB008.csv",sep=",",col.names=TRUE,row.names=FALSE)
+write.table(finalSNPListAllExps,"../results/12.clumpColumn.GWASresults.filtered.ZbrowseReady.ALLtraitsALLexps.csv",sep=",",col.names=TRUE,row.names=FALSE)
